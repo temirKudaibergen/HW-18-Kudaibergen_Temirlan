45b6ddf1a3a80e9c91d960ad4373c0f269a08bc2
temirlan <k.temirlan.t@gmail.com>
2023-05-02 21:54:49 +0600

    Initial Commit

---
+++ HW-18-Kudaibergen_Temirlan.playground/Contents.swift
@@ -0,0 +1,76 @@
+import UIKit
+import PlaygroundSupport
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+// MARK: Initial Data
+
+public struct Chip {
+    public enum ChipType: UInt32 {
+        case small = 1
+        case medium
+        case big
+    }
+
+    public let chipType: ChipType
+
+    public static func make() -> Chip {
+        guard let chipType = Chip.ChipType(rawValue: UInt32(arc4random_uniform(3) + 1)) else {
+            fatalError("Incorrect random value")
+        }
+
+        return Chip(chipType: chipType)
+    }
+
+    public func sodering() {
+        let soderingTime = chipType.rawValue
+        sleep(UInt32(soderingTime))
+    }
+}
+
+// MARK: Properties
+
+private var mutex = NSCondition()
+private var isLock = true
+public var chipStorage: [Chip] = []
+
+//MARK: Object
+
+class GeneratingQueue: Thread {
+    private var runCount = 0
+    
+    func createNewInstance() {
+        var timer = Timer.scheduledTimer(withTimeInterval: 2,
+                                         repeats: true) { timer in
+            mutex.lock()
+            isLock = true
+            chipStorage.append(Chip.make())
+            self.runCount += 1
+            if self.runCount == 5 {
+                timer.invalidate()
+            }
+            do {
+                mutex.signal()
+                mutex.unlock()
+            }
+        }
+    }
+}
+    
+class WorkerQueue: Thread {
+    func solderChip() {
+        mutex.lock()
+        while !isLock {
+            mutex.wait()
+        }
+        isLock = true
+        Chip.sodering(chipStorage.last ?? Chip(chipType: .small))
+        mutex.unlock()
+    }
+}
+
+//MARK: Instances
+
+let generat = GeneratingQueue()
+let work = WorkerQueue()
+generat.createNewInstance()
+work.solderChip()
